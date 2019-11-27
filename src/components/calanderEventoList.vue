222bb9707fc6030ec584e1eddf834ca497f42c29
Aviv Issachar <Avivissachar73@gmail.com>
2019-11-27 20:19:48 +0200

    first creating calander

---
+++ src/components/calanderEventoList.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <ul v-if="eventos">
+            <li v-for="evento in eventos" :key="evento._id">
+                <calander-enento-preview :evento="evento"/>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+import calanderEventoPreview from './calanderEventoPreview.vue';
+
+export default {
+    computed: {
+        eventos() {
+            return this.$store.getters.events.sort((ev1, ev2) => {
+                return (ev1.time.start > ev2.time.start)? 1 : -1;
+            })
+        }
+    },
+    components: {
+        calanderEventoPreview
+    }
+}
+</script>
---
+++ src/components/calanderEventoPreview.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h2>{{date}}.{{month}}, {{day}}</h2>
+        <h3>{{evento.name}}</h3>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['evento'],
+    computed: {
+        month() {
+            return new Date(Date.now()).getMonth()++
+        },
+        date() {
+            return new Date(Date.now()).getDate()
+        },
+        day() {
+            return new Date(Date.now()).getDay()
+        }
+    }
+}
+</script>
