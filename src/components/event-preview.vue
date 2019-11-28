47fcef23aa76c5db2a3a4745c5e0113726cea7aa
Itay Raz <itay.r81@gmail.com>
2019-11-28 14:23:45 +0200

    merge

--- src/components/event-preview.vue
+++ src/components/event-preview.vue
@@ -1,23 +1,31 @@
 <template>
   <section class="evento-preview">
-    <div>{{evento.time.start | new Date(evento.time.start)}}</div>
-    <div>{{evento.name}}</div>
-    <div>{{evento.desc}}</div>
+    <div class="prev-header">
+      <div>{{evento.time.start}}</div>
+      <h2>{{evento.name}}</h2>
+    </div>
+    <short-text :txt="evento.desc" :limit="75"></short-text>
     <div class="preview-btns flex space-around">
     <button @click="removeEvent(evento._id)">X</button>
-    <button @click="removeEvent(evento._id)">Details</button>
+    <router-link :to="'/event/'+evento._id"><button>Details</button></router-link>
     <router-link :to="'/edit/'+evento._id"><button>Edit</button></router-link>
     </div>
   </section>
 </template>
 
 <script>
+
+import shortText from './ShortTxt.vue';
+
 export default {
   props: ["evento"],
   methods: {
     removeEvent(eventoId) {
       this.$store.dispatch({ type: "removeEvent", eventoId });
     }
-  }
+  },
+  components: {
+      shortText
+    }
 };
 </script>
