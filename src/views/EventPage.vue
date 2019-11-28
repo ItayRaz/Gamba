ed1ab75fa74e132f905dfe51a3f1bdb88b74dbe8
Aviv Issachar <Avivissachar73@gmail.com>
2019-11-28 12:22:41 +0200

    first adding filter!

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -1,22 +1,24 @@
 <template>
     <main>
         <evento-filter :categories="eventosCategories" @setFilter="setFilter"/>
-        {{filterBy}}
+        <event-list :eventos="eventosToShow"/>
     </main>
 </template>
 
 <script>
 import eventoFilter from '../components/EventoFilter.vue';
+// import eventList from '../components/EventList.vue';
+import eventList from '../components/CalanderEventoList.vue';
 
 export default {
     data() {
         return {
-            eventos: [],
+            // eventos: [],
             filterBy: {}
         }
     },
     computed: {
-        events() {
+        eventos() {
             return this.$store.getters.eventos;
         },
         eventosCategories() {
@@ -26,6 +28,7 @@ export default {
             var eventosToShow = [...this.eventos];
             var filter = this.filterBy;
 
+            console.log('getting filtered eventos')
             if (filter.searchStr) {
                 eventosToShow = eventosToShow.filter(evento => {
                     return evento.name.toLowerCase().includes(filter.searchStr.toLowerCase) ||
@@ -37,17 +40,18 @@ export default {
                     return filter.category === 'All' || filter.category.toLowerCase() === evento.category.toLowerCase;
                 })
             }
+            console.log('got', eventosToShow)
             return eventosToShow
         }
     },
     methods: {
         setFilter(filterBy) {
-            console.log('filtering!', filterBy);
             this.filterBy = {...filterBy};
         }
     },
     components: {
-        eventoFilter
+        eventoFilter,
+        eventList
     },
     created() {
         this.$store.dispatch('loadEvents');
