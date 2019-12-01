05ed55b51dd23e8e4d4b8f5715708c726cab1df1
Paz Gerti <pazgerti4@gmail.com>
2019-12-01 18:14:26 +0200

    css details

--- src/components/MapDetails.vue
+++ src/components/MapDetails.vue
@@ -4,7 +4,7 @@
       :center="center"
       :zoom="14"
       map-type-id="terrain"
-      style="width:300px;height:200px;"
+      style="width:300px;height:170px;"
       :options="{
    zoomControl: true,
    mapTypeControl: false,
