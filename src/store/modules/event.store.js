7e72f8bc40db70c884e3fe2a7fc1830ce571e7f2
Aviv Issachar <Avivissachar73@gmail.com>
2019-12-01 09:40:31 +0200

    ...

--- src/store/modules/event.store.js
+++ src/store/modules/event.store.js
@@ -74,15 +74,6 @@ export default {
                 .then(eventos => {
                     // context.commit({ type: 'sortEventByDist', data: { eventos, context } });
                     context.commit({ type: 'setEvents', data: { eventos, context } });
-                    
-                    // if (filterBy) {
-                    //     if (filterBy.creatorId) return eventos.filter(evento => {
-                    //         return evento.creator._id === filterBy.creatorId;
-                    //     });
-                    //     if (filterBy.memberId) return eventos.filter(evento => {
-                    //         return evento.members.find(member => member._id === filterBy.memberId);
-                    //     });
-                    // }
                     return eventos;
                 })
         },
