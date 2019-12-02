1c4c592898941afe8301729658a98c6d92714acb
Aviv Issachar <Avivissachar73@gmail.com>
2019-12-02 12:28:37 +0200

    adding avatar to review

--- src/components/ReviewPreview.vue
+++ src/components/ReviewPreview.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="flex align-center wrap review-preview width-all">
-        <router-link v-if="review.reviewer._id !== 'guest'" :to="review.reviewer._id"><img :src="review.reviewer.img" alt=""></router-link>
-        <img v-else :src="review.reviewer.img">
+        <!-- <router-link v-if="review.reviewer._id !== 'guest'" :to="review.reviewer._id"><img :src="review.reviewer.img" alt=""></router-link>
+        <img v-else :src="review.reviewer.img"> -->
+        <router-link v-if="review.reviewer._id !== 'guest'" :to="review.reviewer._id"><avatar class="avatar" :username="review.reviewer.username" :src="review.reviewer.img" :size="50" rounded/></router-link>
+        <avatar v-else class="avatar" :username="review.reviewer._id" :src="review.reviewer.img" :size="50" rounded></avatar>
         <div class="flex column">
             <div class="flex column">
                 <p v-if="review.reviewer._id !== 'guest'"><router-link :to="review.reviewer._id">{{review.reviewer.name}}</router-link></p>
@@ -17,6 +19,8 @@
 </template>
 
 <script>
+import Avatar from "vue-avatar";
+
 export default {
     props: ['review'],
     computed: {
@@ -32,6 +36,9 @@ export default {
         Edit() {
             // this.$store.dispatch({type: 'seReview', _id: this.review._id});
         },
+    },
+    components: {
+        Avatar
     }
 }
 </script>
