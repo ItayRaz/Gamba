faedb2939b6a71cfbfc5991606f0e40c8802aa13
Aviv Issachar <Avivissachar73@gmail.com>
2019-12-04 13:51:34 +0200

    adding default image

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,7 @@
         <router-link to="/"><h1 class="logo">Gam<span>ba</span></h1></router-link>
         <!-- <h3 v-if="logedUser">User: {{logedUser.username}}</h3> -->
         <button class="hamburger" @click="toggleNav">H</button>
-        <ul class="main-nav clean-list flex align-center wrap" @click="toggleNav">
+        <ul class="main-nav clean-list flex align-center wrap">
             <li><router-link class="flex align-center justify-center" to="/event">Events</router-link></li>
             <li><router-link class="flex align-center justify-center" to="/signup/login">{{signupMsg}}</router-link></li>
             <li><router-link class="flex align-center justify-center" to="/about">About</router-link></li>
@@ -27,6 +27,11 @@ export default {
     methods: {
         toggleNav() {
             this.$refs.appHeader.classList.toggle('open');
+        }
+    },
+    watch: {
+        '$route.path'() {
+            this.$refs.appHeader.classList.remove('open');
         }
     }
 }
