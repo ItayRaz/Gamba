4d59c97eea612a0ce53bce2280f47d7aec5e3bb2
Paz Gerti <pazgerti4@gmail.com>
2019-12-05 10:03:50 +0200

    uuj

--- src/components/Creator.vue
+++ src/components/Creator.vue
@@ -1,7 +1,10 @@
 <template>
-  <section v-if="creator" class="creator-container flex column ">
-    <avatar :username="creator.username" :src="creator.img" :size="100" rounded></avatar>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati id possimus sed nobis! Officiis dolorem suscipit ea nobis rerum iste. Temporibus ipsum autem quaerat hic perferendis laboriosam in nesciunt recusandae.</p>
+  <section v-if="user" class="creator-container flex column">
+    <router-link :to="`/user/${user._id}`"><avatar class="user-avatar" :username="creator.username" :src="creator.img" :size="100" rounded></avatar></router-link>
+    <div class="user-contact">
+      <h1>Phone: {{user.mobile}}</h1>
+      <h1>Email: {{user.email}}</h1>
+    </div>
   </section>
 </template>
 
@@ -10,6 +13,14 @@ import Avatar from "vue-avatar";
 
 
 export default {
+    data(){
+      return{
+        user:null
+      }
+    },
+    async created(){
+      this.user = await this.$store.dispatch({type: 'getUser',_id:this.creator._id})
+    },
     components:{
      Avatar,
     },
